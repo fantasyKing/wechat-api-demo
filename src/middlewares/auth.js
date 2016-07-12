db714aab8cfd0039a462c03e638c82c81fb4c01b
zhangGuojin <zhanggjjim@163.com>
2016-07-12 20:52:21 +0800

    增加log

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,35 +3,40 @@ import utility from 'utility';
 
 export default new class {
   validateSign = async (req, res, next) => {
-    const params = await parseParams(req);
-    if (!params.signature) {
-      return res.end('error');
-    }
-    const signature = params.signature;
-    const obj = Object.assign({}, params);
-    if (obj.signature) {
-      delete obj.signature;
-    }
-    if (obj.echostr) {
-      delete obj.echostr;
-    }
-    const arr = [];
-    if (req.method === 'GET') {
-      arr.push('weixin');
-      arr.push(obj.timestamp);
-      arr.push(obj.nonce);
-    } else {
-      for (const key of Object.keys(obj)) {
-        arr.push(obj[key]);
+    try {
+      const params = await parseParams(req);
+      if (!params.signature) {
+        return res.end('error');
       }
+      const signature = params.signature;
+      const obj = Object.assign({}, params);
+      if (obj.signature) {
+        delete obj.signature;
+      }
+      if (obj.echostr) {
+        delete obj.echostr;
+      }
+      const arr = [];
+      if (req.method === 'GET') {
+        arr.push('weixin');
+        arr.push(obj.timestamp);
+        arr.push(obj.nonce);
+      } else {
+        for (const key of Object.keys(obj)) {
+          arr.push(obj[key]);
+        }
+      }
+      const sign = utility.sha1(arr.sort().join(''));
+      if (sign !== signature) {
+        return res.end('error');
+      }
+      if (req.method === 'GET') {
+        return res.end(params.echostr);
+      }
+      logger.info('auth.validateSign===params', params);
+      return next();
+    } catch (err) {
+      return next(err);
     }
-    const sign = utility.sha1(arr.sort().join(''));
-    if (sign !== signature) {
-      return res.end('error');
-    }
-    if (req.method === 'GET') {
-      return res.end(params.echostr);
-    }
-    return next();
   }
 };
