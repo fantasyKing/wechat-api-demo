48b00f999659c9c8002c02658aae7bccb93b97dc
zhangGuojin <zhanggjjim@163.com>
2016-07-12 16:41:25 +0800

    fix bug

--- src/router/api/signature.js
+++ src/router/api/signature.js
@@ -5,9 +5,9 @@ export default new class extends Base {
   checkSignature = async (req, res, params) => {
     try {
       const echostr = await signatureCtrl.checkSignature(params);
-      return res.json(echostr);
+      return res.end(echostr);
     } catch (err) {
-      return res.json(false);
+      return res.end(false);
     }
   }
 };
