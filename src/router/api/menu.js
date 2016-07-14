faaf6af6188d18de07d0c40133d055e18866d63c
zhangGuojin <zhanggjjim@163.com>
2016-07-14 16:55:22 +0800

    增加了扫码，发送位置等菜单

--- src/router/api/menu.js
+++ src/router/api/menu.js
@@ -4,27 +4,81 @@ import Base from './base';
 export default new class extends Base {
 
   createMenu = async (req, res, params) => {
+    // const me = {
+    //   button: [
+    //     {
+    //       type: 'click',
+    //       name: '测试',
+    //       key: 'test'
+    //     },
+    //     {
+    //       name: '菜单',
+    //       sub_button: [
+    //         {
+    //           type: 'view',
+    //           name: '搜索',
+    //           url: 'http://www.baidu.com'
+    //         },
+    //         {
+    //           type: 'view',
+    //           name: '视频',
+    //           url: 'http://v.qq.com/'
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // };
     const me = {
       button: [
         {
-          type: 'click',
-          name: '测试',
-          key: 'test'
-        },
-        {
-          name: '菜单',
+          name: '扫码',
           sub_button: [
             {
-              type: 'view',
-              name: '搜索',
-              url: 'http://www.baidu.com'
+              type: 'scancode_waitmsg',
+              name: '扫码带提示',
+              key: 'scancode_wait'
             },
             {
-              type: 'view',
-              name: '视频',
-              url: 'http://v.qq.com/'
+              type: 'scancode_push',
+              name: '扫码推事件',
+              key: 'scancode_push'
             }
           ]
+        },
+        {
+          name: '发图',
+          sub_button: [
+            {
+              type: 'pic_sysphoto',
+              name: '系统拍照发图',
+              key: 'pic_sysphoto'
+            },
+            {
+              type: 'pic_photo_or_album',
+              name: '拍照或者相册发图',
+              key: 'pic_photo_or_album'
+            },
+            {
+              type: 'pic_weixin',
+              name: '微信相册发图',
+              key: 'pic_weixin'
+            }
+          ]
+        },
+        {
+          name: '发送位置',
+          type: 'location_select',
+          key: 'location_select'
+        },
+        {
+          type: 'media_id',
+          name: '图片',
+          media_id: 'MEDIA_ID1'
+        },
+        {
+          type: 'view_limited',
+          name: '图文消息',
+          media_id: 'MEDIA_ID2'
         }
       ]
     };
