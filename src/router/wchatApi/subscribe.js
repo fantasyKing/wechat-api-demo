e8959260ee84daf7112a0c47aacc5d3dd74ebc4e
zhangGuojin <zhanggjjim@163.com>
2016-12-06 16:06:28 +0800

    fix mpnews

--- src/router/wchatApi/subscribe.js
+++ src/router/wchatApi/subscribe.js
@@ -4,7 +4,7 @@ export default new class {
   subscribe = async (req, res, params) => {
     logger.info('subscribe ===>', params);
     // params.MediaId = 'T8GD0S-0UmutnjdW_gTJHnmupkGaTljPq3T_9wtRGhM';
-    params.MediaId = 'T8GD0S-0UmutnjdW_gTJHtxOionTd4sUAA-DfFS5b_c';
+    params.MediaId = 'T8GD0S-0UmutnjdW_gTJHrLYv_Pb9hBDRRhB2hq4PyA';
     const result = await message.sendMpNews(params);
     logger.info('subscribe.sendMpNews', result);
     return res.end('success');
